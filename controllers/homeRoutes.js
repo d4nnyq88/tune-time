5792adf3ee62ae6ba72bba145b285f18d6da8b8a
Captain America <82297346+d4nnyq88@users.noreply.github.com>
2021-09-28 21:29:21 -0400

    updated handlebars file and homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,4 +9,15 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/profile');
+    //   return;
+    // }
+  
+    res.render('login');
+  });
+
+
 module.exports = router;
