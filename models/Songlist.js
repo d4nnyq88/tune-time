ad9e784876cdce6ffc93d543fa3fe1e4c6d4e33b
Berlic <bsorrell3@gmail.com>
2021-09-28 21:29:44 -0400

    added models

---
+++ models/Songlist.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Songlist extends Model {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Songlist;
