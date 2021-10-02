66e6b849a9464c5489d11da9474e07d101af5030
Rakib Ahmed <rakib987@gmail.com>
2021-10-02 13:25:21 -0400

    song objects created

--- public/js/generate.js
+++ public/js/generate.js
@@ -11,6 +11,28 @@ const durationPlaylist = $(`.playlistDuration`);
 const generateBtn = $(`.submitBtn`);
 promptForm.hide();
 
+// Custom Track Details
+const trackItems = [];
+
+// Custom Track Object
+class Track {
+    constructor(name,duration,artist,id,href,album,external_urls) {
+        this.name = name;
+        this.duration = duration;
+        this.artist = artist;
+        this.id = id;
+        this.href = href;
+        this.album = album;
+        this.external_urls = external_urls;
+    }
+
+    logInfo() {
+        Object.values(this).forEach(value => {
+            console.log(value);
+        })
+    }
+}
+
 // Spotify Token
 let token = ``;
 const clientId = '8f62dc31962f42fabe1974961a756d45';
@@ -72,15 +94,22 @@ function generatePlaylist(title,duration) {
     let durationInS = duration * 60;
     let durationInMs = durationInS * 1000;
 
-    console.log(`The duration of the playlist is ${duration} minutes or ${durationInMs} milliseconds.`);
+    console.log(`The duration of the ${title} playlist is ${duration} minutes or ${durationInMs} milliseconds.`);
 
     fetchTracks().then(tracks => {
-        console.log(tracks);
+        tracks.items.forEach(track => {
+            const {track: {name, duration_ms, artists, id, href, album, external_urls}} = track;
+            const newTrack = new Track(name, duration_ms, artists[0].name,id,href,album,external_urls);
+            trackItems.push(newTrack);
+        })
+        console.log(trackItems);
     })
+
+    return trackItems;
 }
 
 async function fetchTracks() {
-    const response = await fetch(`https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?offset=0&limit=30`, {
+    const response = await fetch(`https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?offset=0&limit=50`, {
         method: 'GET',
         headers: { 'Authorization' : 'Bearer ' + token}
     });
