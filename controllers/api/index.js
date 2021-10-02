00d7ee592d9fb1cd2f76c87172e6f8ceec822a9a
Captain America <82297346+d4nnyq88@users.noreply.github.com>
2021-10-02 11:55:59 -0400

    updated login files and logic

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+// const dashboardRoutes = require('./dashboardRoutes')
 // const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
+// router.use('/dashboard', dashboardRoutes);
 // router.use('/projects', projectRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+// const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
+// router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
